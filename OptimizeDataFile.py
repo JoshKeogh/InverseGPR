e779386f8fba2f720dc1c5e949337fecbe6599fd
JoshKeogh <joshua.keogh@griffithuni.edu.au>
2019-07-28 21:57:41 +1000

    Create OptimizeDataFile.py
    Load raw data from file, convert to and save as numpy array

---
+++ OptimizeDataFile.py
@@ -0,0 +1,34 @@
+import re
+import numpy as np
+
+
+def reformat(xpath, ypath, outpath):
+    xdata = []
+    ydata = []
+    with open(xpath, "r") as x:
+        with open(ypath, "r") as y:
+            for n, yline in enumerate(y):
+                ymatch = re.search(r"-*\d*.?\d+", yline)
+                if ymatch:
+                    ydata.append(ymatch.group())
+                else:
+                    ydata.append(None)
+                row = []
+                while True:
+                    xline = x.readline()
+                    if '----' in xline:
+                        while True:
+                            xline = x.readline()
+                            xmatch = re.findall(r'-*\d.*?\d+', xline)
+                            if len(xmatch) > 2:
+                                row.extend((xmatch[0], xmatch[1], xmatch[2]))
+                            else:
+                                xdata.append(row)
+                                break
+                        break
+
+    np.savez(outpath, np.array(xdata, float), np.array(ydata, float))
+    print("Success")
+
+
+reformat("AllPositions.txt", "AllEnergies.txt", "formatted.npz")
