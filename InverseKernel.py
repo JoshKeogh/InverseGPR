6292e5818715adf2c95de9f7a002221d391657f7
JoshKeogh <joshua.keogh@griffithuni.edu.au>
2019-08-29 11:42:41 +1000

    Update InverseKernel.py

--- InverseKernel.py
+++ InverseKernel.py
@@ -20,39 +20,31 @@ def _pairtype_scale(a_0=None):
 aaa = [0.77, 0.32, 0.32, 0.32, 0.32]
 
 
-def _difference_measure_sq(length_scale, x_left, x_right=None):
-    inv = lambda x: np.triu(1. / np.multiply(x, length_scale), k=1)
-    with np.errstate(divide='ignore'):
-        invR_left = inv(x_left)
-        invR_right = inv(x_right) if x_right is not None else invR_left
-
-    return np.sum((invR_left[:, np.newaxis] - invR_right[np.newaxis, :]) ** 2, axis=(2, 3))
-
-
-def _format(X, Y=None, l_s=None):
-    X = np.reshape(X, (-1, int(len(X[0]) / 3), 3))
-    Y = np.reshape(Y, (-1, int(len(Y[0]) / 3), 3)) if Y is not None else None
-    with np.errstate(divide='ignore'):
-        X = 1. / _near_table(X)
-        Y = 1. / _near_table(Y) if Y is not None else None
-
-    pair_scale = _pairtype_scale(l_s)
-
-    #triu_indices = np.triu_indices_from(pair_scale, k=1)
-
-    return X, Y, pair_scale  #[:, triu_indices[0], triu_indices[1]], \
-           #Y,[:, triu_indices[0], triu_indices[1]] if Y is not None else None, \
-           #pair_scale[triu_indices]
-
-
 class InverseKernel(kernel.RBF):
-    scaler = StandardScaler()
-
     def __call__(self, X, Y=None, eval_gradient=False):
-        X, Y, pair_scale = _format(X, Y, aaa)
+        X = np.reshape(X, (-1, int(len(X[0]) / 3), 3))
+        Y = np.reshape(Y, (-1, int(len(Y[0]) / 3), 3)) if Y is not None else None
+        pair_scale = _pairtype_scale(aaa)
+        with np.errstate(divide='ignore'):
+            X = 1. / (_near_table(X) * pair_scale)
+            Y = 1. / (_near_table(Y) * pair_scale) if Y is not None else None
 
-        dists = _difference_measure_sq(pair_scale, X, Y)
-        K = np.exp(-0.5 * dists)
+        triu_indices = np.triu_indices_from(pair_scale, k=1)
+        X = X[:, triu_indices[0], triu_indices[1]]
+        if Y is None:
+            dists = pdist(X, metric='sqeuclidean')
+            K = np.exp(-.5 * dists)
+            # convert from upper-triangular matrix to square matrix
+            K = squareform(K)
+            np.fill_diagonal(K, 1)
+        else:
+            if eval_gradient:
+                raise ValueError(
+                    "Gradient can only be evaluated when Y is None.")
+
+            Y = Y[:, triu_indices[0], triu_indices[1]]
+            dists = cdist(X, Y, metric='sqeuclidean')
+            K = np.exp(-.5 * dists)
 
         if eval_gradient:
             if self.hyperparameter_length_scale.fixed:
@@ -60,13 +52,13 @@ class InverseKernel(kernel.RBF):
                 return K, np.empty((X.shape[0], X.shape[0], 0))
             elif not self.anisotropic or pair_scale.shape[0] == 1:
                 K_gradient = \
-                    (K * dists)[:, :, np.newaxis]
+                    (K * squareform(dists))[:, :, np.newaxis]
                 return K, K_gradient
             elif self.anisotropic:
                 # We need to recompute the pairwise dimension-wise distances
                 K_gradient = (X[:, np.newaxis, :] - X[np.newaxis, :, :]) ** 2 \
                              / (pair_scale ** 2)
                 K_gradient *= K[..., np.newaxis]
-                return K, -0.5 * K_gradient
+                return K, K_gradient
         else:
             return K
